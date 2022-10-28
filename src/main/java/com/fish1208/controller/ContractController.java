9f5ee169ec60fbbd81151a9f019955c319e0c343
yuchanghong <yuchanghong@grgbanking.com>
2022-10-28 16:07:14 +0800

    fix: 修改

--- src/main/java/com/fish1208/controller/ContractController.java
+++ src/main/java/com/fish1208/controller/ContractController.java
@@ -1,14 +1,21 @@
 package com.fish1208.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.fish1208.common.response.Result;
 import com.fish1208.contract.HelloWorld;
+import io.reactivex.Flowable;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.web3j.protocol.admin.Admin;
+import org.web3j.protocol.core.DefaultBlockParameterName;
+import org.web3j.protocol.core.methods.request.EthFilter;
+import org.web3j.protocol.core.methods.response.Log;
 
+import java.util.Iterator;
 import java.util.Map;
 
 @Slf4j
@@ -21,6 +28,9 @@ public class ContractController {
 
     @Autowired
     private HelloWorld invokeHelloWorld;
+
+    @Autowired
+    private Admin admin;
 
     @RequestMapping(value = "/helloWorld/get", method = RequestMethod.GET)
     public Result<?> get() throws Exception {
@@ -44,6 +54,15 @@ public class ContractController {
                 log.error("HelloWorld set() e: {}", e);
                 return Result.fail("执行HelloWorld合约的set(string)方法失败");
             }
+//            finally {
+//                //设置过滤条件
+//                EthFilter ethFilter = new EthFilter(DefaultBlockParameterName.LATEST, DefaultBlockParameterName.LATEST, "0xc956cFc18B915D412f1Ec04c662891dd604B82F4");
+//                Flowable<Log> flowable = admin.ethLogFlowable(ethFilter);
+//                Iterator<Log> iterator = flowable.blockingIterable().iterator();
+//                while (iterator.hasNext()){
+//                    log.info("Log: {}", JSON.toJSONString(iterator.next()));
+//                }
+//            }
         }
         return Result.fail("HelloWorld合约的invokeHelloWorld对象获取为空");
     }
--- src/main/java/com/fish1208/ethereum/ETHCallback.java
+++ src/main/java/com/fish1208/ethereum/ETHCallback.java
@@ -1,6 +1,7 @@
 package com.fish1208.ethereum;
 
 import cn.hutool.core.collection.CollUtil;
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
 import org.web3j.protocol.core.methods.response.Log;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
@@ -16,14 +17,13 @@ public class ETHCallback implements Callback {
 
     @Override
     public void accept(TransactionReceipt receipt) {
-        String txHash = receipt.getTransactionHash();
-        log.info("txHash: {}", txHash);
-        List<Log> logList = receipt.getLogs();
-        if(CollUtil.isNotEmpty(logList)){
-            for (Log logInfo : logList) {
-                log.info("log data: {}", logInfo.getData());
-            }
-        }
+        log.info("交易明细: {}",  JSON.toJSONString(receipt));
+//        List<Log> logList = receipt.getLogs();
+//        if(CollUtil.isNotEmpty(logList)){
+//            for (Log logInfo : logList) {
+//                log.info("log data: {}", logInfo.getData());
+//            }
+//        }
     }
 
     @Override
